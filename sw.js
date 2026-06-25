06831197d7b5ddef57f8aef4764c4f214e2a624c
Alessio Pace <pacealessio85@gmail.com>
2026-06-25 23:18:32 +0200

    Initial commit - La Bussola di Westeros PWA

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE = "bussola-westeros-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-maskable-512.png",
+  "./icons/apple-touch-icon.png",
+  "./icons/favicon-32.png",
+  "./fonts/cinzel-latin-700-normal.woff2",
+  "./fonts/cinzel-latin-900-normal.woff2",
+  "./fonts/eb-garamond-latin-400-normal.woff2",
+  "./fonts/eb-garamond-latin-400-italic.woff2",
+  "./fonts/eb-garamond-latin-500-normal.woff2",
+  "./fonts/spectral-sc-latin-500-normal.woff2",
+  "./fonts/spectral-sc-latin-600-normal.woff2"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit =>
+      hit || fetch(e.request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return resp;
+      }).catch(() => caches.match("./index.html"))
+    )
+  );
+});
